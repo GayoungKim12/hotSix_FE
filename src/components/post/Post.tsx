1db1b5b4875ea9f3a5b6751d7a71d6724bc7a1d9
jinhyeon0810 <jinhyeon0810@gmail.com>
2023-06-02 00:45:44 +0900

    test: pushtest2

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,5 +1,5 @@
 const Post = () => {
-  return <div className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">Posttttttt</div>;
+  return <div className="mt-4">Post!!!!!!!!!!</div>;
 };
 
 export default Post;
