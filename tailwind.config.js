df3defc68432b0d3842d4f8bfd5ac2aef5771a5f
GayoungKim12 <cloud9b58@gmail.com>
2023-06-05 21:57:53 +0900

    layout: [#001] Custom Color 작성
    Main Color 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{html,tsx,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-100": "#EEF1FF",
+        "main-200": "#D2DAFF",
+        "main-300": "#AAC4FF",
+        "main-400": "#8F92FF",
+      },
+    },
   },
   plugins: [],
 };
