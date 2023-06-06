2a1e964948e0a53e7708203acc70d960cde13602
GayoungKim12 <cloud9b58@gmail.com>
2023-06-06 23:08:06 +0900

    feat: [#007] PostPage 구현
    pages 폴더 내부 PostPage
    components/Post 내부 post 파일 생성

---
+++ src/pages/PostPage.tsx
@@ -0,0 +1,11 @@
+import Post from "../components/Post/Post";
+
+const PostPage = () => {
+  return (
+    <div className="bg-main-100">
+      <Post />
+    </div>
+  );
+};
+
+export default PostPage;
