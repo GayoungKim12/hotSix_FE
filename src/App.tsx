09a033823437bb2171466ca212271fa833d47504
blkaka66 <103988926+blkaka66@users.noreply.github.com>
2023-06-07 16:44:39 +0900

    에러해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import Post from "./components/post/Post";
 import { Routes ,Route,} from 'react-router-dom'
-
-
 import Main from "./pages/Main";
 import ChatLoomPage from "./pages/Chat/ChatLoomPage";
 import ChatPage from "./pages/Chat/ChatListPage";
