b10631016a873cd3d89abb6a566ce2e1123eddf7
blkaka66 <103988926+blkaka66@users.noreply.github.com>
2023-06-07 17:00:15 +0900

    feature:001 chat feat: [#001] 채팅페이지 구현

---
+++ src/components/Chat/ChatLoom/ChatLoomBody.tsx
@@ -0,0 +1,24 @@
+
+
+// const transLateDiv = () => {//채팅 입력창 클릭했을떄 올라오게 하는 함수
+//   console.log("$$$$");
+//   const containerDivs = document.querySelectorAll('.aaaa > div');
+//   containerDivs.forEach((div) => {
+//     div.classList.add(`-translate-y-24`);
+//   });
+// };
+
+const ChatLoomBody = () => {
+  
+ 
+
+  return (
+    <div className="relative flex-grow  ">
+      <div className=" w-full h-full ">
+      <span className="absolute bottom-0">ChatLoomBody</span>
+      </div>
+    </div>    
+  )
+}
+
+export {ChatLoomBody}
