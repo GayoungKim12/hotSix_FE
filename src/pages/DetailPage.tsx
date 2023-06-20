0f04ce39c246a76768f91e814393e67a84710f87
jinhyeon0810 <jinhyeon0810@gmail.com>
2023-06-20 22:00:56 +0900

    feat:[#35] 상세페이지 댓글 수정

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -6,11 +6,29 @@ import CommentToolButtons from "../components/Detail/CommentToolButtons";
 import CommentList from "../components/Detail/CommentList";
 import Header from "../components/Detail/Header";
 import { useParams } from "react-router-dom";
+import { React, useEffect } from "react";
+import { JsonConfig } from "../components/API/AxiosModule";
+import axios from "axios";
+import { atom, useAtom } from "jotai";
+import { showPostButtonsAtom } from "./MainPage";
+
+export const showCommentButtonsAtom = atom(false);
 
 const DetailPage = () => {
   const [showPostButtons, setShowPostButtons] = useState(false);
-  const [showCommentButtons, setShowCommentButtons] = useState(false);
+  const [showCommentButtons, setShowCommentButtons] = useAtom(
+    showCommentButtonsAtom
+  );
+
   const { postId } = useParams();
+  const [commentId, setCommentId] = useState<number | null>();
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    JsonConfig("get", `commentList?post_id=${postId}`, null).then((res) =>
+      setComments(res.data)
+    );
+  }, []);
 
   if (!postId) return null;
 
@@ -18,10 +36,20 @@ const DetailPage = () => {
     <div className="pt-14 min-h-screen bg-main-100">
       <Header />
       <Detail handleShow={() => setShowPostButtons(true)} postId={postId} />
-      <CommentList handleShow={() => setShowCommentButtons(true)} />
+      <CommentList
+        handleShow={() => setShowCommentButtons(true)}
+        comments={comments}
+        setComments={setComments}
+        commentId={commentId}
+        setCommentId={setCommentId}
+      />
       <CommentForm />
-      {showPostButtons && <PostToolButtons postId={postId} handleShow={() => setShowPostButtons(false)} />}
-      {showCommentButtons && <CommentToolButtons handleShow={() => setShowCommentButtons(false)} />}
+      {showPostButtons && (
+        <PostToolButtons
+          postId={postId}
+          handleShow={() => setShowPostButtons(false)}
+        />
+      )}
     </div>
   );
 };
