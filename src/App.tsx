0f40bbfddc74cc746cd55e27d42d3401d2226163
GayoungKim12 <cloud9b58@gmail.com>
2023-06-21 17:01:06 +0900

    feat: [#038] 프로필 페이지 API 구현
    프로필 데이터 가져오기, 무한 스크롤 구현 중

--- src/App.tsx
+++ src/App.tsx
@@ -9,44 +9,38 @@ import ChatPage from "./pages/Chat/ChatListPage";
 import AlarmPage from "./pages/AlarmPage";
 
 import SignUp from "./pages/SignupPage";
-
-
-
-import SigninPage from "./pages/SigninPage";
-
-import { QueryClient , QueryClientProvider } from "react-query";
-import { CookiesProvider } from 'react-cookie';
+import Signin from "./components/Signin/Signin";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { CookiesProvider } from "react-cookie";
 import CartPage from "./pages/CartPage";
 import Editprofile from "./pages/EditprofilePage";
 import Findpassword from "./pages/FindpasswordPage";
 import Editpassword from "./pages/EditpasswordPage";
 
-
 const queryClient = new QueryClient();
 
 function App() {
   return (
-  <CookiesProvider>
-    <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route path="/" element={<MainPage />}></Route>
-        <Route path="/post" element={<PostPage />}></Route>
-        <Route path="/edit/:postId" element={<PostPage />}></Route>
-        <Route path="/detail/:postId" element={<DetailPage />}></Route>
-        <Route path="/cart" element={<CartPage />}></Route>
-        <Route path="/profile" element={<ProfilePage />}></Route>
-        <Route path="/ChatListPage" element={<ChatPage></ChatPage>}></Route>
-        <Route path="/ChatRoom" element={<ChatRoomPage></ChatRoomPage>}></Route>
-        <Route path="/AlarmPage" element={<AlarmPage></AlarmPage>}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
-        <Route path="/signin" element={<SigninPage />}></Route>
-        <Route path="/editprofile" element={<Editprofile/>}></Route>
-        <Route path="/findPassword" element={<Findpassword/>}></Route>
-        <Route path="/Editpassword" element={<Editpassword/>}></Route>
-      </Routes>
-    </QueryClientProvider>
-  </CookiesProvider>
-
+    <CookiesProvider>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/post" element={<PostPage />}></Route>
+          <Route path="/edit/:postId" element={<PostPage />}></Route>
+          <Route path="/detail/:postId" element={<DetailPage />}></Route>
+          <Route path="/cart" element={<CartPage />}></Route>
+          <Route path="/profile/:profileId" element={<ProfilePage />}></Route>
+          <Route path="/ChatListPage" element={<ChatPage></ChatPage>}></Route>
+          <Route path="/ChatRoom" element={<ChatRoomPage></ChatRoomPage>}></Route>
+          <Route path="/AlarmPage" element={<AlarmPage></AlarmPage>}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/editprofile" element={<Editprofile />}></Route>
+          <Route path="/findPassword" element={<Findpassword />}></Route>
+          <Route path="/Editpassword" element={<Editpassword />}></Route>
+        </Routes>
+      </QueryClientProvider>
+    </CookiesProvider>
   );
 }
 
