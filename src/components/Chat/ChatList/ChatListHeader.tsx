76236cc948fdfab1b217bb3e538528435c5c5b5d
GayoungKim12 <cloud9b58@gmail.com>
2023-07-01 22:47:57 +0900

    layout: [#065] 채팅 페이지 수정
    css 수정

--- src/components/Chat/ChatList/ChatListHeader.tsx
+++ src/components/Chat/ChatList/ChatListHeader.tsx
@@ -1,6 +1,6 @@
-import { IoArrowBackOutline } from "react-icons/io5";
-import { useNavigate } from "react-router-dom";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
+import { BiSearch } from "react-icons/bi";
+import GoBackButton from "../../common/GoBackButton";
 
 interface ChatListHeaderProps {
   DeleteMode: () => void;
@@ -9,12 +9,8 @@ interface ChatListHeaderProps {
 }
 
 const ChatListHeader = (props: ChatListHeaderProps) => {
-  const navigate = useNavigate();
   const { filter, setFilter } = props;
-
-  const onClickBackBtn = () => {
-    navigate(-1);
-  };
+  const [search, setSearch] = useState(false);
 
   /* const onClickTrashBtn = () => {
     DeleteMode();
@@ -25,20 +21,28 @@ const ChatListHeader = (props: ChatListHeaderProps) => {
   };
 
   return (
-    <header className="flex flex-col justify-between">
-      <div className="flex justify-between p-4">
-        <div className="flex">
-          <div className="flex justify-center items-center hover:cursor-pointer" onClick={onClickBackBtn}>
-            <IoArrowBackOutline />
+    <>
+      <header className="fixed top-0 flex flex-col justify-between items-center w-full z-50 bg-main-100 shadow-md">
+        <div className="flex justify-between items-center w-full h-16 px-4">
+          <div className="flex">
+            <GoBackButton />
+            <h2 className="ml-4 text-lg">채팅</h2>
           </div>
-          <h2 className="ml-4">채팅</h2>
-        </div>
-        {/* <button className="flex justify-center items-center hover:cursor-pointer hover:border-0 focus:outline-none" onClick={onClickTrashBtn}>
+          <button
+            className="flex justify-center items-center text-2xl hover:cursor-pointer hover:border-0 focus:outline-none"
+            onClick={() => setSearch(!search)}
+          >
+            <BiSearch />
+          </button>
+          {/* <button className="flex justify-center items-center hover:cursor-pointer hover:border-0 focus:outline-none" onClick={onClickTrashBtn}>
           <BsFillTrash3Fill />
         </button> */}
-      </div>
-      <input className="p-2 shadow-sm" value={filter} placeholder="닉네임을 입력하세요" onChange={handleChange}></input>
-    </header>
+        </div>
+      </header>
+      {search && (
+        <input className="fixed top-14 px-4 py-3 shadow-sm w-full z-40" value={filter} placeholder="닉네임을 입력하세요" onChange={handleChange} />
+      )}
+    </>
   );
 };
 
--- src/pages/Chat/ChatListPage.tsx
+++ src/pages/Chat/ChatListPage.tsx
@@ -1,33 +1,19 @@
 import HeaderDeleteMode from "../../components/Chat/ChatList/HeaderDeleteMode";
 import ChatList from "../../components/Chat/ChatList/ChatList";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Footer from "../../components/common/Footer";
 import ChatListHeader from "../../components/Chat/ChatList/ChatListHeader";
-import jwtDecode from "jwt-decode";
+import { getUserId } from "../../components/API/TokenAction";
 
 const ChatListPage = () => {
   const [isDeleteMode, setIsDeleteMode] = useState(false);
   const [allSelect, setAllSelect] = useState(false);
   const [filter, setFilter] = useState("");
-
-  const accessToken = localStorage.getItem("accessToken");
-  const [userId, setUserId] = useState(0);
-
-  useEffect(() => {
-    if (!accessToken) return;
-    const decodeToken = jwtDecode<{ id: string }>(accessToken);
-
-    if (decodeToken.id) {
-      console.log(Number(decodeToken.id));
-      setUserId(Number(decodeToken.id));
-    }
-  }, [accessToken]);
+  const userId = getUserId();
 
   const DeleteMode = () => {
     setIsDeleteMode(!isDeleteMode);
   };
-
-  if (!userId) return <></>;
 
   return (
     <div className="flex flex-col w-full min-h-screen bg-main-100">
@@ -38,7 +24,7 @@ const ChatListPage = () => {
       ) : (
         <>
           <ChatListHeader DeleteMode={DeleteMode} filter={filter} setFilter={setFilter} />
-          <Footer userId={Number(userId)} selected={false} />
+          <Footer userId={userId} selected={false} />
         </>
       )}
       <ChatList isDeleteMode={isDeleteMode} allSelect={allSelect} setAllSelect={setAllSelect} filter={filter} />
