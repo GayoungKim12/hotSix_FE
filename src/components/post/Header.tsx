1a2a38c58c77ccf0cef4d2da6649ed4eb8b061c9
GayoungKim12 <cloud9b58@gmail.com>
2023-07-01 22:51:29 +0900

    layout: [#066] 게시물 작성/수정 페이지 수정
    게시물 작성 수정 페이지 헤더 css 통일

--- src/components/post/Header.tsx
+++ src/components/post/Header.tsx
@@ -3,7 +3,7 @@ import GoBackButton from "../common/GoBackButton";
 const Header = () => {
   return (
     <div>
-      <div className="fixed top-0 flex items-center justify-start px-4 w-full h-14 bg-main-100 shadow-md">
+      <div className="fixed top-0 flex items-center justify-start px-4 w-full h-16 bg-main-100 shadow-md">
         <GoBackButton />
       </div>
     </div>
