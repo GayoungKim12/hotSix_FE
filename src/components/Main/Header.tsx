e23fc59cdb2110fec7e3bc53e317bce98bf5f06b
sow124 <95012722+sow124@users.noreply.github.com>
2023-07-02 21:15:35 +0900

    Update Header.tsx
    ../../public/logo.png을 public/logo.png 로바꿈

--- src/components/Main/Header.tsx
+++ src/components/Main/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <div className="fixed w-full z-20 top-0 left-0 shadow bg-main-100">
       <div className="flex flex-row justify-between items-center h-16 px-3 ">
-        <img src="../../public/logo.png" className="w-16 cursor-pointer" onClick={moveToHome} />
+        <img src="public/logo.png" className="w-16 cursor-pointer" onClick={moveToHome} />
       </div>
     </div>
   );
