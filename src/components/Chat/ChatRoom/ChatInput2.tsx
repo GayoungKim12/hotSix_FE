f1c46bde1d73a02868da81aa29d99f92b7167cff
blkaka66 <103988926+blkaka66@users.noreply.github.com>
2023-07-03 01:41:48 +0900

    Update ChatInput2.tsx

--- src/components/Chat/ChatRoom/ChatInput2.tsx
+++ src/components/Chat/ChatRoom/ChatInput2.tsx
@@ -169,13 +169,13 @@ const ChatInput2 = ({ chatUtil }: { chatUtil: ChatUtil }) => {
   };
 
   return (
-    <div className=" bottom-0 w-full h-12 chat-input-container ">
-      <div className="flex h-full items-center justify-between ">
+    <div className="bottom-0 h-12 chat-input-container ">
+      <div className="flex items-center justify-between ">
         <label className="z-10 rounded-l-2xl h-12 w-8 bg-white ">
           <input type="file" accept="image/*" className="hidden " />
           <span className="relative top-3 left-1">img</span>
         </label>
-        <input ref={newChatRef} onKeyDown={pressEnter} className="flex-grow h-12 px-4 " placeholder="채팅을 입력" />
+        <input ref={newChatRef} onKeyDown={pressEnter} className="h-12 w-full px-4 " placeholder="채팅을 입력" />
         <button onClick={sendMessage} className="rounded-r-2xl h-12 w-10 bg-white  border-black border-l-2  ">
           확인
         </button>
