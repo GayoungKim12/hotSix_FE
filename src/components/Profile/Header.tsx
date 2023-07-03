324593115b6c74cd1e743bd45ebcb39f114e850c
GayoungKim12 <cloud9b58@gmail.com>
2023-07-03 19:06:46 +0900

    layout: [#080] 프로필페이지 CSS 수정
    헤더 css 수정, 자기소개 부분 반응형으로 변경

--- src/components/Profile/Header.tsx
+++ src/components/Profile/Header.tsx
@@ -1,5 +1,5 @@
 import { IoSettingsOutline } from "react-icons/io5";
-import GoBackButton from "../common/GoBackButton";
+import { useNavigate } from "react-router";
 
 interface ProfileProps {
   mypage: boolean;
@@ -7,12 +7,14 @@ interface ProfileProps {
 }
 
 const Header = (props: ProfileProps) => {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <div className="fixed top-0 flex items-center justify-between z-50 px-4 w-full h-16 bg-main-100 shadow-md">
-        <GoBackButton />
+      <div className="fixed top-0 flex items-center justify-between z-50 px-3 w-full h-16 bg-main-100 shadow">
+        <img src="/logo.png" className="w-16 cursor-pointer" onClick={() => navigate("/main")} />
         {props.mypage && (
-          <button className="border-0 text-3xl focus:outline-0 hover:border-0" onClick={props.handleShow}>
+          <button className="border-0 text-3xl text-main-400 focus:outline-0 hover:border-0" onClick={props.handleShow}>
             <IoSettingsOutline />
           </button>
         )}
