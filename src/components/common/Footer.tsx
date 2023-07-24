c91e159d3e0ceef3d1c02522d701fef912711294
GayoungKim12 <cloud9b58@gmail.com>
2023-07-24 21:46:11 +0900

    feat: [#109] 추천페이지 생성
    추천 페이지 css, 기능

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,6 +1,7 @@
 import { HiOutlineChatBubbleLeftRight, HiUserCircle, HiOutlineUserCircle, HiChatBubbleLeftRight } from "react-icons/hi2";
 import { AiOutlineHeart, AiFillHeart, AiOutlineHome, AiFillHome } from "react-icons/ai";
 import { useLocation, useNavigate } from "react-router-dom";
+import { BsHandThumbsUp, BsHandThumbsUpFill } from "react-icons/bs";
 
 interface Props {
   selected: boolean;
@@ -27,20 +28,32 @@ const Footer = ({ userId }: Props) => {
     navigate(`/profile/${userId}`);
   };
 
+  const moveToRecommend = () => {
+    navigate("/recommend");
+  };
+
   return (
     <div className="fixed w-full bottom-0 shadow">
       <article className="flex justify-between items-center h-14 bg-white text-main-300">
-        <div className="flex items-center justify-center w-full text-3xl cursor-pointer" onClick={moveToProfile}>
+        <div className="flex flex-col items-center justify-center w-full text-3xl cursor-pointer" onClick={moveToProfile}>
           {location.pathname.includes("/profile/") ? <HiUserCircle /> : <HiOutlineUserCircle />}
+          <div className="text-xs">프로필</div>
         </div>
-        <div className="flex items-center justify-center w-full text-center text-3xl cursor-pointer" onClick={moveToCart}>
-          {location.pathname.includes("/cart") ? <AiFillHeart className="text-red-400" /> : <AiOutlineHeart />}
+        <div className="flex flex-col items-center justify-center w-full text-center text-3xl cursor-pointer" onClick={moveToCart}>
+          {location.pathname.includes("/cart") ? <AiFillHeart className="" /> : <AiOutlineHeart />}
+          <div className="text-xs">관심목록</div>
         </div>
-        <div className="flex items-center justify-center w-full text-center text-3xl cursor-pointer" onClick={moveToHome}>
+        <div className="flex flex-col items-center justify-center w-full text-center text-3xl cursor-pointer" onClick={moveToHome}>
           {location.pathname.includes("/main") ? <AiFillHome /> : <AiOutlineHome />}
+          <div className="text-xs">홈</div>
         </div>
-        <div className="flex items-center justify-center w-full text-center text-3xl cursor-pointer" onClick={moveToChat}>
+        <div className="flex flex-col items-center justify-center w-full text-center text-3xl cursor-pointer" onClick={moveToRecommend}>
+          {location.pathname.includes("/recommend") ? <BsHandThumbsUpFill /> : <BsHandThumbsUp />}
+          <div className="text-xs">추천</div>
+        </div>
+        <div className="flex flex-col items-center justify-center w-full text-center text-3xl cursor-pointer" onClick={moveToChat}>
           {location.pathname.includes("/chatlist") ? <HiChatBubbleLeftRight /> : <HiOutlineChatBubbleLeftRight />}
+          <div className="text-xs">채팅</div>
         </div>
       </article>
     </div>
