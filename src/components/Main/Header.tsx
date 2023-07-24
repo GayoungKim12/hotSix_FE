4eac08c15bdb1c90edfeb91b4fdcb14099b047a0
blkaka66 <103988926+blkaka66@users.noreply.github.com>
2023-07-24 14:49:17 +0900

    Update Header.tsx

--- src/components/Main/Header.tsx
+++ src/components/Main/Header.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { logoutUser } from "../Profile/SettingButtons";
 const Header = () => {
   const navigate = useNavigate();
-  const [isLogOut , setIsLogOut] = useState<boolean>(false);
+  const [isLogOut, setIsLogOut] = useState<boolean>(false);
   const moveToHome = () => {
     window.scrollTo({ top: 0, behavior: "auto" });
     navigate("/main");
@@ -13,17 +13,26 @@ const Header = () => {
     <div className="fixed w-full z-20 top-0 left-0 shadow bg-main-100">
       <div className="flex flex-row justify-between items-center h-16 px-3 ">
         <img src="/logo.png" className="w-16 cursor-pointer" onClick={moveToHome} />
-        
-          <button className="w-16 h-12 bg-main-400" onClick={(e) => {
-              e.stopPropagation();
-              setIsLogOut(true);
-            }}>로그아웃
-        </button>  
-      
+
+        <button
+          className="w-16 h-12 bg-main-400"
+          onClick={(e) => {
+            e.stopPropagation();
+            setIsLogOut(true);
+          }}
+        >
+          로그아웃
+        </button>
       </div>
-      {isLogOut && <Modal type="confirm" content={"로그 아웃을 하시겠습니까?"} handleModal={() => setIsLogOut(false)} handleDelete={() => logoutUser(navigate)} />}
+      {isLogOut && (
+        <Modal
+          type="confirm"
+          content={"로그 아웃을 하시겠습니까?"}
+          handleModal={() => setIsLogOut(false)}
+          handleDelete={() => logoutUser(navigate)}
+        />
+      )}
     </div>
-    
   );
 };
 
