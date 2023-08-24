8a96f6529ec2885caefcd5e5ced4b378af4e29b1
Jinhyeon Kim <jinhyeon0810@gmail.com>
2023-08-24 17:29:43 +0900

    초기 로딩 시 보일 필요없는 컴포넌트들 코드스플리팅 진행

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,49 @@
-import MainPage from "./pages/MainPage";
 import { Routes, Route } from "react-router-dom";
-import PostPage from "./pages/PostPage";
-import DetailPage from "./pages/DetailPage";
-import ProfilePage from "./pages/ProfilePage";
-import ChatRoomPage from "./pages/Chat/ChatRoomPage";
-import ChatListPage from "./pages/Chat/ChatListPage";
 import { Provider } from "jotai";
-import SignUp from "./pages/SignupPage";
-import SigninPage from "./pages/SigninPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { CookiesProvider } from "react-cookie";
-import CartPage from "./pages/CartPage";
-import Editprofile from "./pages/EditprofilePage";
-import Findpassword from "./pages/FindpasswordPage";
-import Editpassword from "./pages/EditpasswordPage";
-import SocialsignUp from "./pages/SocialsignupPage";
-import GetCode from "./components/Signin/GetCode";
-import RecommendPage from "./pages/RecommendPage";
+import { lazy, Suspense } from "react";
 const queryClient = new QueryClient();
-
+const MainPage = lazy(() => import("./pages/MainPage"));
+const SigninPage = lazy(() => import("./pages/SigninPage"));
+const PostPage = lazy(() => import("./pages/PostPage"));
+const DetailPage = lazy(() => import("./pages/DetailPage"));
+const CartPage = lazy(() => import("./pages/CartPage"));
+const RecommendPage = lazy(() => import("./pages/RecommendPage"));
+const ProfilePage = lazy(() => import("./pages/ProfilePage"));
+const ChatListPage = lazy(() => import("./pages/Chat/ChatListPage"));
+const ChatRoomPage = lazy(() => import("./pages/Chat/ChatRoomPage"));
+const SignUp = lazy(() => import("./pages/SignupPage"));
+const Editprofile = lazy(() => import("./pages/EditprofilePage"));
+const Findpassword = lazy(() => import("./pages/FindpasswordPage"));
+const Editpassword = lazy(() => import("./pages/EditpasswordPage"));
+const SocialsignUp = lazy(() => import("./pages/SocialsignupPage"));
+const GetCode = lazy(() => import("./components/Signin/GetCode"));
 function App() {
   return (
     <Provider>
       <CookiesProvider>
         <QueryClientProvider client={queryClient}>
-          <Routes>
-            <Route path="/" element={<SigninPage />}></Route>
-            <Route path="/login/oauth2/code/kakao" element={<GetCode />}></Route>
-            <Route path="/main" element={<MainPage />}></Route>
-            <Route path="/post" element={<PostPage />}></Route>
-            <Route path="/edit/:postId" element={<PostPage />}></Route>
-            <Route path="/detail/:postId" element={<DetailPage />}></Route>
-            <Route path="/cart" element={<CartPage />}></Route>
-            <Route path="/recommend" element={<RecommendPage />}></Route>
-            <Route path="/profile/:profileId" element={<ProfilePage />}></Route>
-            <Route path="/chatlist" element={<ChatListPage />}></Route>
-            <Route path="/chat/:chatRoomId" element={<ChatRoomPage />}></Route>
-            <Route path="/signup" element={<SignUp />}></Route>
-            <Route path="/editprofile" element={<Editprofile />}></Route>
-            <Route path="/findPassword" element={<Findpassword />}></Route>
-            <Route path="/editpassword" element={<Editpassword />}></Route>
-            <Route path="/socialsignup/:userId" element={<SocialsignUp />}></Route>
-          </Routes>
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Routes>
+              <Route path="/" element={<SigninPage />}></Route>
+              <Route path="/login/oauth2/code/kakao" element={<GetCode />}></Route>
+              <Route path="/main" element={<MainPage />}></Route>
+              <Route path="/post" element={<PostPage />}></Route>
+              <Route path="/edit/:postId" element={<PostPage />}></Route>
+              <Route path="/detail/:postId" element={<DetailPage />}></Route>
+              <Route path="/cart" element={<CartPage />}></Route>
+              <Route path="/recommend" element={<RecommendPage />}></Route>
+              <Route path="/profile/:profileId" element={<ProfilePage />}></Route>
+              <Route path="/chatlist" element={<ChatListPage />}></Route>
+              <Route path="/chat/:chatRoomId" element={<ChatRoomPage />}></Route>
+              <Route path="/signup" element={<SignUp />}></Route>
+              <Route path="/editprofile" element={<Editprofile />}></Route>
+              <Route path="/findPassword" element={<Findpassword />}></Route>
+              <Route path="/editpassword" element={<Editpassword />}></Route>
+              <Route path="/socialsignup/:userId" element={<SocialsignUp />}></Route>
+            </Routes>
+          </Suspense>
         </QueryClientProvider>
       </CookiesProvider>
     </Provider>
